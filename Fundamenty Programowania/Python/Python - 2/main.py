a2fe2bb6831598a2d1e74d78d6b8beb895d7b015
Fremen1990 <fremen1990@gmail.com>
2021-02-03 04:08:00 +0100

    next commit

--- Fundamenty Programowania/Python/Python - 2/main.py
+++ Fundamenty Programowania/Python/Python - 2/main.py
@@ -1,3 +1,21 @@
 print("Hello Python!! ")
+print("- - "*50)
 
-if __name
+class Task:
+    def __init__(self, name:str):
+        self.name=name
+        self.is_done=False
+
+
+
+class Aplication:
+    def __init__(self):
+        self.tasks=[]
+
+    def show(self):
+        for task in self.tasks:
+           print(task.show())
+
+if __name__ == '__main__':
+    aplication=Aplication()
+    aplication.show()
